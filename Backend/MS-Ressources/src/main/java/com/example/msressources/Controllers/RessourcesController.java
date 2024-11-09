260e24200cb11746ba47a8b20fbd8091ba04f611
Nourkhalfaoui <nour.khalfaoui@esprit.tn>
2024-11-09 23:02:07 +0100

    CRUD

--- Backend/MS-Ressources/src/main/java/com/example/msressources/Controllers/RessourcesController.java
+++ Backend/MS-Ressources/src/main/java/com/example/msressources/Controllers/RessourcesController.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("ressources")
 @AllArgsConstructor
 public class RessourcesController {
     RessourcesService ressourcesService;
